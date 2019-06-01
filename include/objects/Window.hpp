4864340b71a9a364e0fb28e4e65aa20a289ab1c8
Alonso Zamorano <alonso.zamorano97@gmail.com>
2019-06-01 12:31:11 -0400

    A texture can be drawn now.

--- include/objects/Window.hpp
+++ include/objects/Window.hpp
@@ -3,29 +3,36 @@
 
 #include <map>
 #include <string>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 #include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Window {
 public:
-        // Data
-        SDL_Window* window;
+	// Data
+	SDL_Window* window;
 	SDL_GLContext context;
-        map<string, SDL_Texture*> textures;
+	map<string, unsigned int> textures;
+	int shader_program;
+	unsigned int VAO;
+	unsigned int vert_buffer;
 
 
-        // Methods
-        Window(const char *title, int x, int y, int width, int height, bool fullscreen);
-        ~Window();
+	// Methods
+	Window(const char *title, int x, int y, int width, int height, bool fullscreen);
+	~Window();
 
-        void clear();
-        void update();
-        void close();
-        void load_texture(const char *path);
-        void render(const char* texture_path, SDL_Rect clip, SDL_Rect dst);
+	void clear();
+	void update();
+	void close();
+	void load_texture(const char *path);
+	void render(const char* texture_path, SDL_Rect clip, SDL_Rect dst);
 };
 
 #endif
--- src/objects/Window.cpp
+++ src/objects/Window.cpp
@@ -1,4 +1,60 @@
 #include "objects/Window.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+/* Useful functions
+   =================== */
+string get_text(string path) {
+	string res, str = "";
+
+	ifstream in;
+	in.open (path.c_str());
+	if (in.is_open()) {
+		getline (in, str);
+		while (in) {
+			res += str + "\n";
+			getline (in, str);
+		}
+		return res;
+	} else {
+		cerr << "Unable to open file \"" << path << "\"\n";
+		throw 2;
+	}
+}
+
+static unsigned int load_shader(string path) {
+	string source = get_text(path);
+
+	// Get type of shader
+	unsigned int shader;
+	if (path.substr(path.find_last_of (".") + 1) == "vert") {
+		shader = glCreateShader(GL_VERTEX_SHADER);
+
+	} else {
+		shader = glCreateShader(GL_FRAGMENT_SHADER);
+	}
+
+	// Compile shader
+	const char *source_as_c_str = source.c_str();
+	glShaderSource(shader, 1, &source_as_c_str, NULL);
+	glCompileShader(shader);
+
+	// Check if was compiled correctly
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if(!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		cout <<  "Shader in \"" << path << "\" could not be compilated :(\n";
+	}
+
+	return shader;
+}
+
+
+/* Window
+   ====== */
 
 Window::Window(const char *title, int x, int y, int width, int height, bool fullscreen) {
 	// Initialize SDL
@@ -39,15 +95,77 @@ Window::Window(const char *title, int x, int y, int width, int height, bool full
 
 	// V-Sync
 	SDL_GL_SetSwapInterval(1);
+
+	// Create shader program
+	unsigned int vertex_shader = load_shader("shaders/vertex_shader.vert");
+	unsigned int fragment_shader = load_shader("shaders/fragment_shader.frag");
+	this->shader_program = glCreateProgram();
+	glAttachShader(this->shader_program, vertex_shader);
+	glAttachShader(this->shader_program, fragment_shader);
+	glLinkProgram(this->shader_program);
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+
+
+	// Define data
+	// -----------
+	float vertices[] = {
+	    // positions         // colors           // texture coords
+	    0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+	    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+	   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+	   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+	};
+	unsigned int indices[] = {
+	    0, 1, 3, // first triangle
+	    1, 2, 3  // second triangle
+	};
+
+	// Create Vertex Array Object to store configuration
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	// Create buffer in VRAM for vertices
+	unsigned int vert_buffer;
+	glGenBuffers(1, &vert_buffer); // Create vertex buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vert_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Put data on the buffer
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); // Specify layout of positions
+	glEnableVertexAttribArray(0);  // Tell OpenGL we can use location 0 in shaders
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float))); // Specify layout of colors
+	glEnableVertexAttribArray(1); // Tell OpenGL we can use location 1 in shaders
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float))); // Specify layout of texture coordinates
+	glEnableVertexAttribArray(2); // Tell OpenGL we can use location 1 in shaders
+
+	// Create buffer in VRAM for indices
+	unsigned int ind_buffer;
+	glGenBuffers(1, &ind_buffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ind_buffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Load texture
+	load_texture("assets/texture_atlas.png");
+
+	glUniform1i(glGetUniformLocation(shader_program, "ourTexture"), 0); // set it manually
 }
 
 Window::~Window() {
 
 }
 
+
 void Window::clear() {
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(shader_program);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textures["assets/texture_atlas.png"]);
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 void Window::update() {
@@ -61,11 +179,30 @@ void Window::close() {
 }
 
 void Window::load_texture(const char* path) {
-	/*SDL_Surface *temp_surface = IMG_Load(path);
-	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, temp_surface);
-	SDL_FreeSurface(temp_surface);
+	unsigned int texture;
 
-	textures.insert(pair<string, SDL_Texture*>(path, texture));*/
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// Set texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	// Load image
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	} else {
+		cout << "Failed to load texture :(\n";
+	}
+	stbi_image_free(data);
+
+	textures.insert(pair<string, unsigned int>(path, texture));
 }
 
 void Window::render(const char* texture_path, SDL_Rect clip, SDL_Rect dst) {
