5b15d1a559e95c023553e6917fe5f3ce34f9d87c
Alonso Zamorano <alonso.zamorano97@gmail.com>
2019-08-12 12:01:48 -0400

    Update

--- include/objects/Window.hpp
+++ include/objects/Window.hpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 #include "SDL2/SDL.h"
-#include <glad/glad.h>
+#include <deps/glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
--- src/objects/Window.cpp
+++ src/objects/Window.cpp
@@ -1,6 +1,6 @@
 #include "objects/Window.hpp"
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include <deps/stb_image.h>
 
 /* Window
    ====== */
@@ -44,61 +44,6 @@ Window::Window(const char *title, int x, int y, int width, int height, bool full
 
 	// V-Sync
 	SDL_GL_SetSwapInterval(1);
-
-	// Create shader program
-	unsigned int vertex_shader = load_shader("shaders/vertex_shader.vert");
-	unsigned int fragment_shader = load_shader("shaders/fragment_shader.frag");
-	this->shader_program = glCreateProgram();
-	glAttachShader(this->shader_program, vertex_shader);
-	glAttachShader(this->shader_program, fragment_shader);
-	glLinkProgram(this->shader_program);
-	glDeleteShader(vertex_shader);
-	glDeleteShader(fragment_shader);
-
-
-	// Define data
-	// -----------
-	float vertices[] = {
-	    // positions         // colors           // texture coords
-	    0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-	    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-	   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-	   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
-	};
-	unsigned int indices[] = {
-	    0, 1, 3, // first triangle
-	    1, 2, 3  // second triangle
-	};
-
-	// Create Vertex Array Object to store configuration
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	// Create buffer in VRAM for vertices
-	unsigned int vert_buffer;
-	glGenBuffers(1, &vert_buffer); // Create vertex buffer
-	glBindBuffer(GL_ARRAY_BUFFER, vert_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Put data on the buffer
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); // Specify layout of positions
-	glEnableVertexAttribArray(0);  // Tell OpenGL we can use location 0 in shaders
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float))); // Specify layout of colors
-	glEnableVertexAttribArray(1); // Tell OpenGL we can use location 1 in shaders
-
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float))); // Specify layout of texture coordinates
-	glEnableVertexAttribArray(2); // Tell OpenGL we can use location 1 in shaders
-
-	// Create buffer in VRAM for indices
-	unsigned int ind_buffer;
-	glGenBuffers(1, &ind_buffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ind_buffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// Load texture
-	load_texture("assets/texture_atlas.png");
-
-	glUniform1i(glGetUniformLocation(shader_program, "ourTexture"), 0); // set it manually
 }
 
 Window::~Window() {
@@ -122,5 +67,9 @@ void Window::close() {
 }
 
 void Window::render(const char* texture_path, SDL_Rect clip, SDL_Rect dst) {
+
+}
+
+void Window::load_texture(const char *path) {
 	
 }
